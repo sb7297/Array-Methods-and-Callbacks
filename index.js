416b95ab7c6c14eccf96ca76a4e0b3165ab19cfa
sb7297 <sbaireddy7297@gmail.com>
2021-06-15 11:25:25 -0400

    index.js: complete task 3

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, getFinals) {
+    return getFinals(data).map(e => e.Year);
 }
 
 
